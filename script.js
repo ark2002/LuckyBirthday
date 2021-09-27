1a07a6ab3d619f618a93830ed51d92e7e75501b9
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2021-09-27 17:24:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const dateOfBirth = document.querySelector("#IDoB");
+const luckyNumber = document.querySelector("#ILN");
+const submit = document.querySelector("#submit");
+const outputText = document.querySelector("#outputText");
+const outputGif = document.querySelector("#outputGif");
+
+
+function compareValues(sum,luckyNumber)
+{
+  if(sum%luckyNumber===0){
+    outputText.innerText="You have a Lucky Birthday";
+    outputGif.src ="/gif/lucky.gif";
+  }else
+  {
+   outputText.innerText="You have a Unlucky Birthday";
+   outputGif.src ="/gif/unlucky.gif";
+  }
+}
+
+
+function checkLuckyBirthday(){
+  const dob =dateOfBirth.value
+  const sum = calcSum(dob);
+  if(sum&&dob)
+  compareValues(sum,luckyNumber.value)
+  else{
+  outputText.innerText="Please enter both the fields";
+  outputGif.src ="/gif/unlucky.gif";
+  }
+}
+function calcSum(dob){
+  dob = dob.replaceAll("-","")
+  let sum=0;
+  for(let i=0;i<dob.length;i++){
+    sum = sum+Number(dob.charAt(i))
+  }
+  return sum
+}
+
+
+submit.addEventListener("click",function getValues(){
+  checkLuckyBirthday()
+})
